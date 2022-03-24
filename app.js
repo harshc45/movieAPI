96be05f21f7e41991f6371c8d08a8dba1de31364
Harshwardhan Chougule <harshc466@gmail.com>
2022-03-25 01:01:53 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+//connect to the database
+mongoose.connect("mongodb+srv://Harshwardhan:Harshwardhan@cluster0.x9vm5.mongodb.net/moviesDB?retryWrites=true&w=majority", {useNewUrlParser: true});
+
+//create the schema
+const userSchema = {
+    firstname: String,
+    lastname: String,
+    moviename: String,
+    seat: Number,
+    popcorn: Boolean
+}
+
+//create the model
+const UserDB = mongoose.model("UserDB", userSchema);
+
+app.get("/bookings", function(req, res){
+    UserDB.find(function(err, foundUsers){
+        if(!err){
+            res.send(foundUsers);;
+        }
+        else{
+            res.send(err);
+        }
+
+    });
+});
+
+app.get("/bookings/:fname/",  function(req, res){
+    UserDB.findOne({firstname: req.params.fname}, function(err, foundUser){
+        if(!err){
+            res.send(foundUser);
+        }
+        else{
+            res.send(err);
+        }
+    });
+});
+app.get("/bookings",  function(req, res){
+    UserDB.find( function(err, foundUser){
+        if(!err){
+            res.send(foundUser);
+        }
+        else{
+            res.send(err);
+        }
+    });
+});
+
+app.post("/book", function(req, res){
+    const newData = new UserDB({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        moviename: req.body.moviename,
+        seat: req.body.seat,
+        popcorn: req.body.popcorn
+    });
+
+    newData.save(function(err){
+        if(!err){
+            res.send("Successfully booked");
+        }
+        else{
+            res.send(err);
+        }
+    });
+});
+
+app.delete("/cancelBooking", function(req, res){
+    UserDB.deleteOne({firstname: req.body.firstname}, function(err){
+        if(!err){
+            res.send("Successfully cancelled");
+        }
+        else{
+            res.send(err);
+        }
+    })
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000.");
+})
