5c50bdf869bdb991452c7e339993fc4ce46b456d
Harshwardhan Chougule <harshc466@gmail.com>
2022-03-25 19:21:50 +0530

    changes

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.delete("/cancelBooking", function(req, res){
 let port = process.env.PORT;
 if(port == null || port == ""){
     port = 3000;
-}
+} 
 
 app.listen(port, function(){
     console.log("Server started on port 3000.");
